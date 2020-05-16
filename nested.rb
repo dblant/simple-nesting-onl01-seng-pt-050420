1f344bc41fe3bf5f369e9c50909d725f39f1bd1b
Dustin Blanton <dustinblntn@gmail.com>
2020-05-16 04:39:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+     programmer_hash[:dennis_ritchie][:languages]
 
 def adding_matz
 # add the following information to the top level of programmer_hash
